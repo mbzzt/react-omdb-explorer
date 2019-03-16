9313c562f1b8b6c2edb229186ee7db9da9337de9
Martin Bozzuto <martin.bozzuto@rockstarcoders.com>
2019-03-16 00:01:13 -0300

    updated readme, solved issues with dependencies, fixed issues suggested by eslint, prettified code

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
-# react-omdb-explorer
-React Open Movie Database Explorer
+# React Open Movie Database Explorer
+
+## Getting started
+
+1. Clone the repository
+2. Run `yarn install` from command line
+3. Get a free OMDb API key from here: https://www.omdbapi.com/apikey.aspx
+4. Create a `.env` file in the root directory. Save your OMDb API key in the `.env` file:
+   ```sh
+   REACT_APP_OMDB_API_KEY=12345678
+   ```
+5. Start the development server, running `yarn start` from command line
--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
-import React, { Component } from "react";
-import "./App.css";
+import React, { Component } from 'react';
+import './App.css';
 
 const DEFAULT_QUERY = 'fight';
 // const DEFAULT_HPP = '100';
@@ -10,7 +10,7 @@ const DEFAULT_QUERY = 'fight';
 // const PARAM_PAGE = 'page=';
 // const PARAM_HPP = 'hitsPerPage=';
 
-const API_KEY = '12345678'; // your free api key from omdbapi.com
+const API_KEY = process.env.REACT_APP_OMDB_API_KEY; // your free api key from omdbapi.com, set in .env file
 const PATH_BASE = 'http://www.omdbapi.com/';
 const PARAM_API_KEY = 'apikey=';
 const PARAM_SEARCH = 's=';
@@ -26,38 +26,23 @@ class App extends Component {
       searchKey: '',
       searchTerm: DEFAULT_QUERY,
       error: null,
-      activeMovie: null,
+      activeMovie: null
     };
-
-    this.needsToSearchTopStories = this.needsToSearchTopStories.bind(this);
-    this.setSearchTopStories = this.setSearchTopStories.bind(this);
-    this.fetchSearchTopStories = this.fetchSearchTopStories.bind(this);
-    this.needsToSearchMovie = this.needsToSearchMovie.bind(this);
-    this.setSearchMovie = this.setSearchMovie.bind(this);
-    this.fetchMovie = this.fetchMovie.bind(this);
-    this.onGetMovieInfo = this.onGetMovieInfo.bind(this);
-    this.onResetActiveMovie = this.onResetActiveMovie.bind(this);
-    this.onDismiss = this.onDismiss.bind(this);
-    this.onSearchChange = this.onSearchChange.bind(this);
-    this.onSearchSubmit = this.onSearchSubmit.bind(this);
   }
 
-  needsToSearchTopStories(searchTerm) {
-    return !this.state.results[searchTerm];
+  componentDidMount() {
+    const { searchTerm } = this.state;
+    this.setState({ searchKey: searchTerm });
+    this.fetchSearchTopStories(searchTerm);
   }
 
-  setSearchTopStories(result) {
+  setSearchTopStories = result => {
     const { movies, page } = result;
     const { searchKey, results } = this.state;
 
-    const oldHits = results && results[searchKey]
-      ? results[searchKey].movies
-      : [];
+    const oldHits = results && results[searchKey] ? results[searchKey].movies : [];
 
-    const updatedHits = [
-      ...oldHits,
-      ...movies
-    ];
+    const updatedHits = [...oldHits, ...movies];
 
     this.setState({
       results: {
@@ -65,22 +50,11 @@ class App extends Component {
         [searchKey]: { movies: updatedHits, page }
       }
     });
-  }
+  };
 
-  fetchSearchTopStories(searchTerm, page = 1) {
-    fetch(`${PATH_BASE}?${PARAM_API_KEY}${API_KEY}&${PARAM_SEARCH}${searchTerm}&${PARAM_PAGE}${page}`)
-      .then(response => response.json())
-      .then(result => this.setSearchTopStories({ movies: result.Search, page}))
-      .catch(e => this.setState({ error: e }));
-  }
-
-  needsToSearchMovie(imdbID) {
-    return !(this.state.movies && this.state.movies[imdbID]);
-  }
-
-  setSearchMovie(result) {
+  setSearchMovie = result => {
     const { movies } = this.state;
-    const imdbID = result.imdbID;
+    const { imdbID } = result;
 
     this.setState({
       movies: {
@@ -89,39 +63,60 @@ class App extends Component {
       }
     });
 
-    console.log(this.state.movies[imdbID]);
-  }
+    console.log(movies[imdbID]);
+  };
 
-  fetchMovie(imdbID, page = 1) {
+  needsToSearchMovie = imdbID => {
+    const { movies } = this.state;
+    return !(movies && movies[imdbID]);
+  };
+
+  fetchSearchTopStories = (searchTerm, page = 1) => {
+    fetch(
+      `${PATH_BASE}?${PARAM_API_KEY}${API_KEY}&${PARAM_SEARCH}${searchTerm}&${PARAM_PAGE}${page}`
+    )
+      .then(response => response.json())
+      .then(result => this.setSearchTopStories({ movies: result.Search, page }))
+      .catch(e => this.setState({ error: e }));
+  };
+
+  needsToSearchTopStories = searchTerm => {
+    const { results } = this.state;
+    return !results[searchTerm];
+  };
+
+  fetchMovie = imdbID => {
     return fetch(`${PATH_BASE}?${PARAM_API_KEY}${API_KEY}&${PARAM_ID}${imdbID}`)
       .then(response => response.json())
       .then(result => this.setSearchMovie(result))
       .catch(e => this.setState({ error: e }));
-  }
+  };
 
-  onGetMovieInfo(imdbID) {
+  onGetMovieInfo = imdbID => {
     console.log(`get movie info for: ${imdbID}`);
+    const { movies } = this.state;
     if (this.needsToSearchMovie(imdbID)) {
-      this.fetchMovie(imdbID)
-        .then(() => this.setState({
-          activeMovie: this.state.movies[imdbID]
-        }));
+      this.fetchMovie(imdbID).then(() =>
+        this.setState({
+          activeMovie: movies[imdbID]
+        })
+      );
     } else {
-      console.log(this.state.movies[imdbID]);
+      console.log(movies[imdbID]);
       this.setState({
-        activeMovie: this.state.movies[imdbID]
+        activeMovie: movies[imdbID]
       });
     }
-  }
+  };
 
-  onResetActiveMovie(event) {
+  onResetActiveMovie = event => {
     event.preventDefault();
     this.setState({
-      activeMovie: null,
+      activeMovie: null
     });
-  }
+  };
 
-  onDismiss(id) {
+  onDismiss = id => {
     const { searchKey, results } = this.state;
     const { movies, page } = results[searchKey];
 
@@ -134,15 +129,15 @@ class App extends Component {
         [searchKey]: { movies: updatedHits, page }
       }
     });
-  }
+  };
 
-  onSearchChange(event) {
+  onSearchChange = event => {
     this.setState({
       searchTerm: event.target.value
     });
-  }
+  };
 
-  onSearchSubmit(event) {
+  onSearchSubmit = event => {
     const { searchTerm } = this.state;
     this.setState({
       error: null,
@@ -152,88 +147,53 @@ class App extends Component {
       this.fetchSearchTopStories(searchTerm);
     }
     event.preventDefault();
-  }
-
-  componentDidMount() {
-    const { searchTerm } = this.state;
-    this.setState({ searchKey: searchTerm });
-    this.fetchSearchTopStories(searchTerm);
-  }
+  };
 
   render() {
-    const {
-      searchTerm,
-      results,
-      searchKey,
-      error,
-      activeMovie
-    } = this.state;
+    const { searchTerm, results, searchKey, error, activeMovie } = this.state;
 
-    const page = (
-      results &&
-      results[searchKey] &&
-      results[searchKey].page
-    ) || 1;
+    const page = (results && results[searchKey] && results[searchKey].page) || 1;
 
-    const list = (
-      results &&
-      results[searchKey] &&
-      results[searchKey].movies
-    ) || [];
+    const list = (results && results[searchKey] && results[searchKey].movies) || [];
 
     return (
       <div className="page">
         <div className="interactions">
-          <Search
-            value={searchTerm}
-            onChange={this.onSearchChange}
-            onSubmit={this.onSearchSubmit}
-          >
+          <Search value={searchTerm} onChange={this.onSearchChange} onSubmit={this.onSearchSubmit}>
             Search
           </Search>
         </div>
-        {error
-          ? <div className="interactions">
+        {error ? (
+          <div className="interactions">
             <p>Something went wrong.</p>
           </div>
-          : <Table
+        ) : (
+          <Table
             list={list}
             onDismiss={this.onDismiss}
             onGetMovieInfo={this.onGetMovieInfo}
             onResetActiveMovie={this.onResetActiveMovie}
             activeMovie={activeMovie}
           />
-        }
+        )}
         <div className="interactions">
-          <Button onClick={() => this.fetchSearchTopStories(searchKey, page + 1)}>
-            More
-          </Button>
+          <Button onClick={() => this.fetchSearchTopStories(searchKey, page + 1)}>More</Button>
         </div>
       </div>
     );
   }
 }
 
-const Search = ({
-  value,
-  onChange,
-  onSubmit,
-  children
-}) =>
+const Search = ({ value, onChange, onSubmit, children }) => (
   <form onSubmit={onSubmit}>
-    <input
-      type="text"
-      value={value}
-      onChange={onChange}
-    />
-    <button type="submit">
-      {children}
-    </button>
+    <input type="text" value={value} onChange={onChange} />
+    <button type="submit">{children}</button>
   </form>
+);
 
-const Table = ({ list, onDismiss, onGetMovieInfo, onResetActiveMovie, activeMovie }) =>
+const Table = ({ list, onDismiss, onGetMovieInfo, onResetActiveMovie, activeMovie }) => (
   <div className="table" id="close">
-    {list.map(item =>
+    {list.map(item => (
       <div key={item.imdbID} className="table-row">
         <span style={{ width: '40%' }}>
           <a href={item.url}>{item.Title}</a>
@@ -241,16 +201,10 @@ const Table = ({ list, onDismiss, onGetMovieInfo, onResetActiveMovie, activeMovi
         <span style={{ width: '20%' }}>{item.Year}</span>
         <span style={{ width: '20%' }}>{item.Type}</span>
         <span style={{ width: '20%' }}>
-          <Button
-            onClick={() => onDismiss(item.imdbID)}
-            className="button-inline"
-          >
+          <Button onClick={() => onDismiss(item.imdbID)} className="button-inline">
             Dismiss
           </Button>
-          <Button
-            onClick={() => onGetMovieInfo(item.imdbID)}
-            className="button-inline"
-          >
+          <Button onClick={() => onGetMovieInfo(item.imdbID)} className="button-inline">
             Info
           </Button>
         </span>
@@ -259,40 +213,61 @@ const Table = ({ list, onDismiss, onGetMovieInfo, onResetActiveMovie, activeMovi
           // <span></span>
         }
       </div>
-    )}
-    {activeMovie &&
-      <div className={'modal ' + (activeMovie ? 'active' : '')} id="modal-movie">
-        <a href="" className="modal-overlay" aria-label="Close" onClick={onResetActiveMovie}><span className="text-assistive">Close</span></a>
+    ))}
+    {activeMovie && (
+      <div className={`modal ${activeMovie ? 'active' : ''}`} id="modal-movie">
+        <button
+          type="button"
+          className="modal-overlay"
+          aria-label="Close"
+          onClick={onResetActiveMovie}
+        >
+          <span className="text-assistive">Close</span>
+        </button>
         <div className="modal-container">
           <div className="modal-header">
-            <button className="btn btn-clear float-right" aria-label="Close" onClick={onResetActiveMovie}><span className="text-assistive">Close</span></button>
+            <button
+              type="button"
+              className="btn btn-clear float-right"
+              aria-label="Close"
+              onClick={onResetActiveMovie}
+            >
+              <span className="text-assistive">Close</span>
+            </button>
             <div className="modal-title h5">Modal title</div>
           </div>
           <div className="modal-body">
             <div className="content">
               <ul>
-                { Object.keys(activeMovie).map(k =>
-                    k !== 'Poster'
-                      ? <li key={k.toLowerCase()}>{k + ": " + activeMovie[k]}</li>
-                      : <li key={k.toLowerCase()}>
-                          {k + ": "}<br />
-                          <img src={activeMovie[k]} alt={activeMovie.Title} />
-                        </li>
+                {Object.keys(activeMovie).map(k =>
+                  k !== 'Poster' ? (
+                    <li key={k.toLowerCase()}>{`${k}: ${activeMovie[k]}`}</li>
+                  ) : (
+                    <li key={k.toLowerCase()}>
+                      {`${k}: `}
+                      <br />
+                      <img src={activeMovie[k]} alt={activeMovie.Title} />
+                    </li>
+                  )
                 )}
               </ul>
             </div>
           </div>
           <div className="modal-footer">
-            <button className="btn btn-primary" onClick={onResetActiveMovie}>Close</button>
+            <button type="button" className="btn btn-primary" onClick={onResetActiveMovie}>
+              Close
+            </button>
           </div>
         </div>
       </div>
-    }
+    )}
   </div>
+);
 
-const Button = ({ onClick, className = "", children }) =>
+const Button = ({ onClick, className = '', children }) => (
   <button onClick={onClick} className={className} type="button">
     {children}
   </button>
+);
 
 export default App;
